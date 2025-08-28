e50092692fb3350d684be07b0d45585a14f82229
Hermann <188955756+4483-s@users.noreply.github.com>
2025-08-28 15:18:44 +0800

    HashMap methods

--- linkedlist.js
+++ linkedlist.js
@@ -1,30 +1,21 @@
-function LinkedList() {
-  let headNode = null;
-  let size = 0;
-  const prepend = (key, value) => {
-    const newNode = Node(key, value);
-    if (!headNode) {
-      headNode = newNode;
-    } else {
-      newNode.nextNode = headNode;
-      headNode = newNode;
-    }
-    size++;
-  };
-  const getSize = () => {
+const linkedList = (function () {
+  //accept a head node of a linked list, if head is falsy, return 0
+  const getSize = (head) => {
     let count = 0;
-    let currentNode = headNode;
+    let currentNode = head;
     while (currentNode) {
       count++;
       currentNode = currentNode.nextNode;
     }
     return count;
   };
-  const at = (n) => {
+  //return the node at index n
+  const at = (head, n) => {
+    const size = getSize(head);
     if (n >= size || n < 0) {
       return undefined;
     }
-    let currentNode = headNode;
+    let currentNode = head;
     for (let i = 0; i <= n; i++) {
       if (i === n) {
         return currentNode;
@@ -32,43 +23,24 @@ function LinkedList() {
       currentNode = currentNode.nextNode;
     }
   };
-  const append = (key, value) => {
-    const newNode = Node(key, value);
-    if (!size) {
-      headNode = newNode;
-    } else {
-      at(size - 1).nextNode = newNode;
-    }
-    size++;
+  //only works if the head is a node object
+  const append = (head, newNode) => {
+    at(head, getSize(head) - 1).nextNode = newNode;
   };
-  const pop = () => {
-    if (!size) return;
-    if (size === 1) {
-      headNode = null;
-    } else {
-      let target = at(size - 2);
-      target.nextNode = null;
-    }
-    size--;
-  };
-  const toString = () => {
-    let result = "";
+  const findByKey = (head, key) => {
+    const size = getSize(head);
     for (let i = 0; i < size; i++) {
-      result += `( ${at(i).key}  ${at(i).value}) -> `;
-    }
-    return result + "null";
-  };
-  const containsKey = (key) => {
-    for (let i = 0; i < size; i++) {
-      if (at(i).key === key) {
-        return true;
+      const currentNode = at(head, i);
+      if (currentNode.key === key) {
+        return currentNode;
       }
     }
-    return false;
+    return null;
   };
+  // not refactored
   const find = (key) => {
     for (let i = 0; i < size; i++) {
-      if (at(i).key === key) {
+      if ((head, at(i).key === key)) {
         return i;
       }
     }
@@ -110,26 +82,11 @@ function LinkedList() {
 
   return {
     append,
-    prepend,
     at,
-    pop,
-    toString,
-    containsKey,
-    find,
-    insertAt,
-    removeAt,
-    get head() {
-      return headNode;
-    },
-    get tail() {
-      const v = at(size - 1);
-      return v ? v : null;
-    },
-    get size() {
-      return size;
-    },
+    getSize,
+    findByKey,
   };
-}
+})();
 function Node(key, value) {
   return { key, value, nextNode: null };
 }
@@ -137,30 +94,57 @@ function Node(key, value) {
 //
 let loadFactor = 3 / 4;
 function HashMap() {
-  const buckets = new Array(16);
+  let buckets = new Array(16 * 4);
+  //utils
+  //
   const getGrowNum = () => buckets.length * loadFactor;
+  const checkAndGrow = () => {
+    const num = getGrowNum();
+    const len = length();
+    if (len >= num) {
+      buckets = [...buckets, ...new Array(buckets.length).fill(null)];
+    }
+  };
+  function hash(key) {
+    let hashCode = 0;
+    const primeNumber = 97;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % buckets.length;
+    }
+    return hashCode;
+  }
+  function tempTest() {
+    for (const b of buckets) {
+      console.log(b);
+    }
+  }
+  // HashMap methods
+  //
+  const set = (key, value) => {
+    const bIndex = hash(key);
+    if (!buckets[bIndex]) {
+      buckets[bIndex] = Node(key, value);
+    } else if (linkedList.findByKey(buckets[bIndex], key)) {
+      const targetNode = linkedList.findByKey(buckets[bIndex], key);
+      targetNode.value = value;
+    } else {
+      const newNode = Node(key, value);
+      linkedList.append(buckets[bIndex], newNode);
+    }
+    checkAndGrow();
+  };
+  //count keys in the whole hashtable
   const length = () => {
     let count = 0;
     for (const buk of buckets) {
-      if (buk) {
-        count += buk.getSize();
-      }
+      count += linkedList.getSize(buk);
     }
     return count;
   };
   const clear = () => {
-    buckets.fill();
+    buckets.fill(null);
   };
-  function hash(key) {
-    let hashCode = 0;
-
-    const primeNumber = 97;
-    for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
-      console.log(hashCode);
-    }
-
-    return hashCode;
-  }
-  return { hash };
+  return { hash, clear, set, tempTest, length };
 }
+let n = HashMap();
+console.log(n.hash("a"));
