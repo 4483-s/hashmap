08cf800eac0573a79fbd26ab97ba4ca78430b985
Hermann <188955756+4483-s@users.noreply.github.com>
2025-08-29 16:56:03 +0800

    Finish required functionality

--- linkedlist.js
+++ linkedlist.js
@@ -11,10 +11,6 @@ const linkedList = (function () {
   };
   //return the node at index n
   const at = (head, n) => {
-    const size = getSize(head);
-    if (n >= size || n < 0) {
-      return undefined;
-    }
     let currentNode = head;
     for (let i = 0; i <= n; i++) {
       if (i === n) {
@@ -46,39 +42,6 @@ const linkedList = (function () {
     }
     return null;
   };
-  const insertAt = (key, value, index) => {
-    if (index < 0) {
-      console.error("Fatal Error: Invalid index, exiting.");
-      return;
-    }
-    const newNode = Node(key, value);
-    if (!headNode) {
-      headNode = newNode;
-      size++;
-      return;
-    }
-    const preNode = at(index - 1);
-    if (at(index)) {
-      newNode.nextNode = at(index);
-    }
-    if (preNode) {
-      preNode.nextNode = newNode;
-    } else if (size === 1) {
-      headNode = newNode;
-    } else {
-      at(size - 1).nextNode = newNode;
-    }
-    size++;
-  };
-  const removeAt = (index) => {
-    if (index === 0) {
-      headNode = headNode.nextNode;
-      size--;
-    } else if (at(index)) {
-      at(index - 1).nextNode = at(index + 1) ? at(index + 1) : null;
-      size--;
-    }
-  };
 
   return {
     append,
@@ -87,64 +50,225 @@ const linkedList = (function () {
     findByKey,
   };
 })();
-function Node(key, value) {
-  return { key, value, nextNode: null };
+// function Node(key, value) {
+//   return { key, value, nextNode: null };
+// }
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.nextNode = null;
+  }
+  forEach(callback) {
+    callback(this);
+    if (this.nextNode) {
+      this.nextNode.forEach(callback);
+    }
+  }
+  getLength() {
+    let count = 0;
+    let currentNode = this;
+    while (currentNode) {
+      count++;
+      currentNode = currentNode.nextNode;
+    }
+    return count;
+  }
+  append(newNode) {
+    let currentNode = this;
+    while (currentNode.nextNode) {
+      currentNode = currentNode.nextNode;
+    }
+    currentNode.nextNode = newNode;
+  }
+  find(key) {
+    if (this.key === key) {
+      return this;
+    } else if (this.nextNode) {
+      return this.nextNode.find(key);
+    } else {
+      return null;
+    }
+  }
+  removeByKey(key) {
+    if (this.key === key) {
+      return this.nextNode;
+    }
+    let currentNode = this;
+    while (currentNode.nextNode) {
+      if (currentNode.nextNode.key === key) {
+        currentNode.nextNode = currentNode.nextNode.nextNode;
+        return this;
+      }
+      currentNode = currentNode.nextNode;
+    }
+    return false;
+  }
+  get length() {
+    return this.getLength();
+  }
 }
 
 //
 let loadFactor = 3 / 4;
+// hashmap factory start
 function HashMap() {
-  let buckets = new Array(16 * 4);
+  let buckets = new Array(16).fill(null);
+  let capacity = buckets.length;
   //utils
   //
   const getGrowNum = () => buckets.length * loadFactor;
-  const checkAndGrow = () => {
+  const checkGrow = () => {
     const num = getGrowNum();
     const len = length();
-    if (len >= num) {
-      buckets = [...buckets, ...new Array(buckets.length).fill(null)];
-    }
+    return len >= num;
   };
+  const grow = () => {
+    capacity = capacity * 2;
+    let newArr = new Array(capacity).fill(null);
+    for (const b of buckets) {
+      if (b) {
+        b.forEach((v) => {
+          setNewItem(newArr, v.key, v.value);
+        });
+      }
+    }
+    buckets = newArr;
+  };
+  const setNewItem = (arr, key, value, checkExsisting = false) => {
+    const bIndex = hash(key);
+    if (!arr[bIndex]) {
+      arr[bIndex] = new Node(key, value);
+      return;
+    }
+    //if checkExsisting is true, check if the key already exists, if so, update the value and exit, otherwise, append to the bucket
+    if (checkExsisting) {
+      const foundKey = arr[bIndex].find(key);
+      if (foundKey) {
+        foundKey.value = value;
+        return;
+      }
+    }
+    const newNode = new Node(key, value);
+    arr[bIndex].append(newNode);
+  };
+  //hash function
   function hash(key) {
     let hashCode = 0;
     const primeNumber = 97;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % buckets.length;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
     }
     return hashCode;
   }
+  // show hashmap
   function tempTest() {
     for (const b of buckets) {
       console.log(b);
     }
   }
+  //
   // HashMap methods
   //
   const set = (key, value) => {
-    const bIndex = hash(key);
-    if (!buckets[bIndex]) {
-      buckets[bIndex] = Node(key, value);
-    } else if (linkedList.findByKey(buckets[bIndex], key)) {
-      const targetNode = linkedList.findByKey(buckets[bIndex], key);
-      targetNode.value = value;
-    } else {
-      const newNode = Node(key, value);
-      linkedList.append(buckets[bIndex], newNode);
+    if (checkGrow()) {
+      grow();
     }
-    checkAndGrow();
+    setNewItem(buckets, key, value, true);
   };
   //count keys in the whole hashtable
   const length = () => {
     let count = 0;
     for (const buk of buckets) {
-      count += linkedList.getSize(buk);
+      if (buk) count += buk.length;
     }
     return count;
   };
   const clear = () => {
     buckets.fill(null);
   };
-  return { hash, clear, set, tempTest, length };
+  const getBucket = () => buckets;
+  const values = () => {
+    const arr = [];
+    for (const b of buckets) {
+      if (b) {
+        b.forEach((v) => {
+          arr.push(v.value);
+        });
+      }
+    }
+    return arr;
+  };
+  const entries = () => {
+    const arr = [];
+    for (const b of buckets) {
+      if (b) {
+        b.forEach((v) => {
+          arr.push([v.key, v.value]);
+        });
+      }
+    }
+    return arr;
+  };
+  const keys = () => {
+    const arr = [];
+    for (const b of buckets) {
+      if (b) {
+        b.forEach((v) => {
+          arr.push(v.key);
+        });
+      }
+    }
+    return arr;
+  };
+  const has = (key) => {
+    for (const b of buckets) {
+      if (b) {
+        const foundKey = b.find(key);
+        if (foundKey) return true;
+      }
+    }
+    return false;
+  };
+  const get = (key) => {
+    for (const b of buckets) {
+      if (b) {
+        const foundKey = b.find(key);
+        if (foundKey) {
+          return foundKey.value;
+        }
+      }
+    }
+    return null;
+  };
+  const remove = (key) => {
+    for (let i = 0; i < buckets.length; i++) {
+      if (buckets[i]) {
+        const result = buckets[i].removeByKey(key);
+        if (typeof result === "object") {
+          buckets[i] = result;
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+  return {
+    remove,
+    hash,
+    clear,
+    set,
+    tempTest,
+    length,
+    getBucket,
+    get,
+    has,
+    values,
+    entries,
+    keys,
+  };
 }
-let n = HashMap();
-console.log(n.hash("a"));
+let m = HashMap();
+for (let i = 0; i < 30; i++) {
+  m.set(`${Math.random() * 100}`, `${Math.random() * 700}`);
+}
